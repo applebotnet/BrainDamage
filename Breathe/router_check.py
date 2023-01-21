0af48689a89a5d133308c8fe5157c6eec4f64d3d
applebotnet <122900315+applebotnet@users.noreply.github.com>
2023-01-21 13:02:38 -0800

    Update router_check.py

--- Breathe/router_check.py
+++ Breathe/router_check.py
@@ -24,14 +24,14 @@ class FindRouter():
         try:
             return socket.gethostbyname(socket.gethostname())
         except Exception as e:
-            print e
+            print (e)
 
 
     def getPublicIp(self):
         try:
             return urllib2.urlopen('http://ip.42.pl/raw').read()
         except Exception as e:
-            print e
+            print (e)
 
 
     def getRouterList(self):
@@ -45,7 +45,7 @@ class FindRouter():
         if ip_to_check[0] == '192' and ip_to_check[1] == '168':
             if int(ip_to_check[2]) >= 0 and int(ip_to_check[2]) <= 255:
                 if int(ip_to_check[3]) >= 0 and int(ip_to_check[3]) <= 255:
-                    print "[*] IP: %s is NAT'd" % (".".join(ip_to_check))
+                    print ("[*] IP: %s is NAT'd") % (".".join(ip_to_check))
                     return True
                     
                     
@@ -53,20 +53,20 @@ class FindRouter():
             if int(ip_to_check[1]) >= 0 and int(ip_to_check[1]) <= 255:
                 if int(ip_to_check[2]) >= 0 and int(ip_to_check[2]) <= 255:
                     if int(ip_to_check[3]) >= 0 and int(ip_to_check[3]) <= 255:
-                        print "[*] IP: %s is NAT'd" % (".".join(ip_to_check))
+                        print ("[*] IP: %s is NAT'd") % (".".join(ip_to_check))
                         return True
                         
         if ip_to_check[0] == '172':
             if int(ip_to_check[1]) >= 16 and int(ip_to_check[1]) <= 31:
                 if int(ip_to_check[2]) >= 0 and int(ip_to_check[2]) <= 255:
                     if int(ip_to_check[3]) >= 0 and int(ip_to_check[3]) <= 255:
-                        print "[*] IP: %s is NAT'd" % (".".join(ip_to_check))
+                        print ("[*] IP: %s is NAT'd") % (".".join(ip_to_check))
                         return True
                         
         if ip_to_check[0] == '169' and ip_to_check[1] == '254':
             if int(ip_to_check[2]) >= 0 and int(ip_to_check[2]) <= 255:
                 if int(ip_to_check[3]) >= 0 and int(ip_to_check[3]) <= 255:
-                    print "[*] IP: %s is NAT'd (APIPA only)" % (".".join(ip_to_check))
+                    print ("[*] IP: %s is NAT'd (APIPA only)") % (".".join(ip_to_check))
                     return True
                     
         return False
@@ -83,7 +83,7 @@ class FindRouter():
             for line in ipconfig_output:
                 if 'Default Gateway' in line and ip_to_check[:dot_pos[1]+1] in line:
                     gatewayip = line[line.index(ip_to_check[ip_to_check.index(ip_to_check[0])]):].rstrip()
-                    print '[*] Gateway IP: ' + gatewayip
+                    print ('[*] Gateway IP: ') + gatewayip
                     return gatewayip
 
 
@@ -107,7 +107,7 @@ class FindRouter():
                 router_webpage = response.split()
             
             except Exception as e:
-                print '[*] Failed connection on host', host, 'port', ports
+                print ('[*] Failed connection on host'), host, 'port', ports
         return True
         
         
@@ -121,7 +121,7 @@ class FindRouter():
             response = urllib2.urlopen(r).read()
             router_webpage = response.split()
         except Exception as e:
-            print '[*] Failed connection on host', url
+            print ('[*] Failed connection on host'), url
         return True
     
     
@@ -132,13 +132,13 @@ class FindRouter():
         for i in router_list:
             for j in router_webpage:
                 if i.lower() in j.lower():
-                    print "[*] Router Found: %s" % i
+                    print ("[*] Router Found: %s") % i
                     return i
         return False
            
            
     def run(self):
-        print '[*] Looking for router'
+        print ('[*] Looking for router')
         host = self.pingDefaultGateway() #'127.0.0.1'
         open_ports = self.openPorts(host, 50, 10000) # [7777,8888]
         self.getRouterList()
