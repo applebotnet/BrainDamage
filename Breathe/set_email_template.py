b7511c05cea0ec6c9f50283f88691184f1f1bfdc
applebotnet <122900315+applebotnet@users.noreply.github.com>
2023-01-21 13:03:26 -0800

    Update set_email_template.py

--- Breathe/set_email_template.py
+++ Breathe/set_email_template.py
@@ -9,13 +9,13 @@ PDF Attachment:  update_Form.pdf'''
 
 
 def setDefaultEmailTemplate(PATH_MAIL_TEMP):
-    print '[*] Setting default email template'
+    print ('[*] Setting default email template')
     template_file = open(PATH_MAIL_TEMP,'w')
     template_file.write(text)
     template_file.close()
     
 def setEmailTemplate(PATH_MAIL_TEMP, URL):
-    print '[*] Setting email template'
+    print ('[*] Setting email template')
     data = urllib2.urlopen(URL)
     data = data.split("\n")
     template_file = open(PATH_MAIL_TEMP,'w')
@@ -23,7 +23,7 @@ def setEmailTemplate(PATH_MAIL_TEMP, URL):
     template_file.close()
     
 def updateEmailTemplate(PATH_MAIL_TEMP, data):
-    print '[*] Updating email template'
+    print ('[*] Updating email template')
     template_file = open(PATH_MAIL_TEMP,'w')
     template_file.write(data)
     template_file.close()
