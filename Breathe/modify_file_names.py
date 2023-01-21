6e4e962e251196cfb1b558016987a9016ac83922
applebotnet <122900315+applebotnet@users.noreply.github.com>
2023-01-21 12:55:49 -0800

    Update modify_file_names.py

--- Breathe/modify_file_names.py
+++ Breathe/modify_file_names.py
@@ -4,10 +4,10 @@ import os
 # thisFile = __file__
 
 def local_file(path,filename,newname):
-    print '[*] Modifying local file'
+    print ('[*] Modifying local file')
     os.rename(os.path.join(path,filename),os.path.join(path,newname))
     
 def myself(thisFile,newname):
-    print '[*] Modifying itself'
+    print ('[*] Modifying itself')
     os.rename(thisFile,newname)
     
